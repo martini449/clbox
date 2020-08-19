96c96fb544429e9cc8bdb741fe03ff7c3ed0b96c
Grzegorz Lipecki <glipecki@consdata.pl>
2020-08-19 09:23:11 +0200

    fixes permission errors on logout and first login

--- apps/web-app/src/app/features/inbox/state/fetch-inbox/fetch-inbox.epic.ts
+++ apps/web-app/src/app/features/inbox/state/fetch-inbox/fetch-inbox.epic.ts
@@ -1,30 +1,39 @@
 import firebase from 'firebase';
 import {Epic} from 'redux-observable';
-import {combineLatest, Observable} from 'rxjs';
-import {distinct, filter, map, switchMap, takeUntil, tap} from 'rxjs/operators';
+import {combineLatest, Observable, of} from 'rxjs';
+import {distinct, map, switchMap} from 'rxjs/operators';
 import {AppState} from '../../../../state/app-state';
 import {loggedIn} from '../../../authentication/state/login/logged-in.action';
-import {loggedOut} from '../../../authentication/state/logout/logged-out.action';
 import {firebaseApp} from '../../../firebase/firebase.app';
 import {InboxMessage} from '../model/inbox-message';
 import {InboxMessageState} from '../model/inbox-message-state';
 import {inboxFetched} from './inbox-fetched';
 
 const firestore = firebaseApp.firestore();
-export const fetchInboxEpic: Epic<ReturnType<typeof loggedIn>, any, AppState> = (action$, state$) => combineLatest([action$.ofType(loggedIn.type), state$])
-  .pipe(
-    filter(([_, state]) => state.team?.current?.id !== undefined),
-    distinct(([action, state]) => `${action.payload.email}/${state.team.current.id}`),
-    switchMap(([action, state]) => new Observable<firebase.firestore.QuerySnapshot>(subscriber => {
-      const inboxCollection = firestore.collection(`team/${state.team.current.id}/inbox/${action.payload.email}/message`);
-      inboxCollection.onSnapshot(subscriber);
-    })),
-    takeUntil(action$.ofType(loggedOut)),
-    map(messages => inboxFetched({
-      messages: messages.docs.map(doc => (<InboxMessage>{
-        id: doc.id,
-        state: InboxMessageState.Pending,
-        ...doc.data()
-      }))
-    })),
-  );
+export const fetchInboxEpic: Epic<ReturnType<typeof loggedIn>, any, AppState> = (action$, state$) => combineLatest([
+  state$.pipe(
+    map(state => state.authentication?.email)
+  ),
+  state$.pipe(
+    map(state => state.team?.current?.id)
+  )
+]).pipe(
+  distinct(([user, team]) => `${user}/${team}`),
+  switchMap(([user, team]) => {
+    if (team && user) {
+      return new Observable<firebase.firestore.QuerySnapshot>(subscriber => {
+        const inboxCollection = firestore.collection(`team/${team}/inbox/${user}/message`);
+        inboxCollection.onSnapshot(subscriber);
+      });
+    } else {
+      return of<firebase.firestore.QuerySnapshot>();
+    }
+  }),
+  map(messages => inboxFetched({
+    messages: messages.docs.map(doc => (<InboxMessage>{
+      id: doc.id,
+      state: InboxMessageState.Pending,
+      ...doc.data()
+    }))
+  })),
+);
--- apps/web-app/src/app/features/stats/state/fetch-stats/fetch-stats.epic.ts
+++ apps/web-app/src/app/features/stats/state/fetch-stats/fetch-stats.epic.ts
@@ -1,29 +1,37 @@
 import firebase from 'firebase';
 import {Epic} from 'redux-observable';
-import {combineLatest, Observable} from 'rxjs';
-import {filter, map, switchMap, tap, withLatestFrom} from 'rxjs/operators';
+import {combineLatest, Observable, of} from 'rxjs';
+import {distinct, map, switchMap} from 'rxjs/operators';
 import {AppState} from '../../../../state/app-state';
 import {loggedIn} from '../../../authentication/state/login/logged-in.action';
 import {firebaseApp} from '../../../firebase/firebase.app';
-import {teamsFetched} from '../../../team/state/teams-fetched';
 import {Stats} from '../stats';
 import {statsFetched} from './stats-fetched';
 
 const firestore = firebaseApp.firestore();
 const currentMonth = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '').substring(0, 7);
-export const fetchStatsEpic: Epic<ReturnType<typeof loggedIn>, any, AppState> = (action$, state$) => {
-  return action$
-    .ofType(teamsFetched.type)
-    .pipe(
-      withLatestFrom(state$),
-      filter(([_, state]) => !state.stats.months[currentMonth]),
-      switchMap(([_, state]) => new Observable<firebase.firestore.DocumentSnapshot>(subscriber => {
-        const monthStats = firestore.collection(`team/${state.team.current.id}/stats`).doc(currentMonth);
-        monthStats.onSnapshot(subscriber)
-      })),
-      map(stats => statsFetched({
-        month: currentMonth,
-        stats: stats.data() as Stats
-      }))
-    );
-};
+export const fetchStatsEpic: Epic<ReturnType<typeof loggedIn>, any, AppState> = (action$, state$) =>
+  combineLatest([
+    state$.pipe(
+      map(state => state.authentication?.email)
+    ),
+    state$.pipe(
+      map(state => state.team?.current?.id)
+    )
+  ]).pipe(
+    distinct(([user, team]) => `${user}/${team}`),
+    switchMap(([user, team]) => {
+      if (user && team) {
+        return new Observable<firebase.firestore.DocumentSnapshot>(subscriber => {
+          const monthStats = firestore.collection(`team/${team}/stats`).doc(currentMonth);
+          monthStats.onSnapshot(subscriber)
+        });
+      } else {
+        return of<firebase.firestore.DocumentSnapshot>();
+      }
+    }),
+    map(stats => statsFetched({
+      month: currentMonth,
+      stats: stats.data() as Stats
+    }))
+  );
